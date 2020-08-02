2e52fac7819f61e58345af544a8334933de6b1c1
cnails <andreyandreevgr@gmail.com>
2020-08-02 04:17:02 +0300

    a

--- Code/flask_application.py
+++ Code/flask_application.py
@@ -8,4 +8,5 @@ if __name__ == '__main__':
     # load_model('BERT_JOINT_model_ru')
     # load_model('BERT_GRAN_model_ru')
     load_model('BERT_MULTIGRAN_model_sigmoid_ru')
+    # load_model('BERT_ru')
     app.run(debug=True, use_reloader=False)
